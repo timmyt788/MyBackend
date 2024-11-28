0f906422fc86e4cadda54d528a7e6d84bc95f775
timmyt788 <ruthrich788@outlook.com>
2024-11-28 06:34:54 -0500

    added cors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const userRouter = require("./routes/userRouter");
+const cors = require("cors");
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.get("/", (req, res) => {
